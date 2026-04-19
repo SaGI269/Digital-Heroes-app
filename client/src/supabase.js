1a2c86c2056f3e50e3db0ce664fd085683816ba9
Jembia Sagar <jembiasagar@gmail.com>
2026-04-19 21:06:14 +0530

    update login/signup flow

--- client/src/supabase.js
+++ client/src/supabase.js
@@ -1,8 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 );
 
 export default supabase;
