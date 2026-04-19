1c70515a7f282c5c09fb2455845f5d271b98eb7d
Jembia Sagar <jembiasagar@gmail.com>
2026-04-19 22:28:31 +0530

    fix: build issue and env setup

--- client/src/supabase.js
+++ client/src/supabase.js
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
 
-dotenv.config();
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
